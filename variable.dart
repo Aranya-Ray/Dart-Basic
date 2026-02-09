b929c05b9fb1905c15cdd93ebb221c885676b747
Aranya Ray <aranyaray10@gmail.com>
2026-02-09 20:40:01 +0600

    Update variable.dart

--- variable.dart
+++ variable.dart
@@ -8,3 +8,30 @@ void main(){
  print("Iam $name.I Lives in $location.my age is $age.I have $tk tk.my hight is $hight.my relationship status is $single");
  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+void main() {
+  String name = "Himel";
+  String address = "BD";
+  num age = 25;
+  num height = 5.9;
+  bool isMarried = false;
+
+  print("Name is $name");
+  print("Address is $address");
+  print("Age is $age");
+  print("Height is $height");
+  print("Married Status is $isMarried");
+}
+
