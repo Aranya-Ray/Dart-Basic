96882719b783a2361a239ea24cdf6ce1665e54e6
Aranya Ray <aranyaray10@gmail.com>
2026-02-09 20:33:35 +0600

    Create variable.dart

---
+++ variable.dart
@@ -0,0 +1,10 @@
+void main(){
+ String name = "Arpo";
+ String location = "khulna";
+ int age = 21;
+ int tk = 200;
+ double hight = 5.6;
+ bool  single = true;
+ print("Iam $name.I Lives in $location.my age is $age.I have $tk tk.my hight is $hight.my relationship status is $single");
+ 
+}
