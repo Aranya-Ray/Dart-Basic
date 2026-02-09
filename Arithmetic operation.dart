3e4382eb5503ac3742a8a610deaa3d37593f7528
Aranya Ray <aranyaray10@gmail.com>
2026-02-09 16:38:48 +0600

    Create Arithmetic operation.dart

---
+++ Arithmetic operation.dart
@@ -0,0 +1,22 @@
+void main() {
+    int number1 = 100;
+    int number2 = 200;
+    int sum = (number1+number2);
+    print (sum);
+    
+    int sub = (number1-number2);
+    print(sub);
+    
+    int a = 50;
+    int mul = (a*a);
+    print(mul);
+    
+
+    double div = (number1/a);
+    print(div);
+    
+    double u = 10.5;
+    double o = 20.5;
+    double jk =(u+o);
+    print(jk);
+}
