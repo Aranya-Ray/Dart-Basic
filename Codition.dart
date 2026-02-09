e185bd1f831d7ee6364d075bc1ab6734bf817ee3
Aranya Ray <aranyaray10@gmail.com>
2026-02-09 21:22:06 +0600

    Create Codition.dart

---
+++ Codition.dart
@@ -0,0 +1,48 @@
+void main()
+{
+    int age = 15;
+    
+    if(age >= 12){
+      print("You are Adult.");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void main(){
+    double hight = 5.5;
+    if (hight < 6 ){
+        print("you are Short");
+    }
+}
+
+void main(){
+    double hight = 2.0;
+    if (hight > 7 ){
+        print("you are toll");
+    }else if(hight == 2.0){
+        print("you are baby");
+    } else{
+        print("you are short");
+    }
+}
+
+
+
