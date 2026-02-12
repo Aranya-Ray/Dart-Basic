159a9e51fe29ff1fa36229365808a8d185b42fc8
Aranya Ray <aranyaray10@gmail.com>
2026-02-12 23:54:26 +0600

    Create arrow function.dart

---
+++ arrow function.dart
@@ -0,0 +1,31 @@
+double ui ( int a, int n)=>
+    (a / n);
+    
+
+void main(){
+    double oo = ui(56,78);
+    print (oo);
+}
+
+
+
+
+
+double ui ( int a, int n)=>
+    (a / n);
+
+void main(){
+    double oo = ui(56,78);
+    print (oo);
+}
+
+
+
+
+
+int fj (int a, int v) =>
+     (a+v);
+void main (){
+    int fh = fj (5647,4569);
+    print(fh);
+}
