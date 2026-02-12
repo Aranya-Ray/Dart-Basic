f28160961aa1b59a1d56f5f0286bdc168b62e68a
Aranya Ray <aranyaray10@gmail.com>
2026-02-12 13:42:40 +0600

    Create function.dart

---
+++ function.dart
@@ -0,0 +1,7 @@
+void pk (String a , String b, String c, int d, double x){
+    print('My Detailes $a $b $c weight $d height $x');
+    print(d);
+}
+void main(){
+    pk ("Arpo","Roy", 'yash', 20, 50.5);
+}
