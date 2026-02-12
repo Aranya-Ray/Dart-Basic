1de3a0fe88771478eb0cb5cfc36ec4cc23f42930
Aranya Ray <aranyaray10@gmail.com>
2026-02-13 00:15:36 +0600

    Create For loop.dart

---
+++ For loop.dart
@@ -0,0 +1,26 @@
+void main(){
+  for(int i=10; i>=1; i--){
+    print('$i ');
+  }
+  
+}
+
+
+
+
+void main (){
+    for (int i=20; i<=50; i++){
+        print ("$i");
+    }
+}
+
+
+
+
+
+void main(){
+    for (int a=50;  a>=30; a--){
+        print("$a");
+    }
+    
+}
