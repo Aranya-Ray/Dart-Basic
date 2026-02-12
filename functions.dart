38626d79aa67bcd83f27ab0e6e34ea1a42b1aa55
Aranya Ray <aranyaray10@gmail.com>
2026-02-12 14:03:57 +0600

    Update functions.dart

--- functions.dart
+++ functions.dart
@@ -5,3 +5,17 @@ void pk (String a , String b, String c, int d, double x){
 void main(){
     pk ("Arpo","Roy", 'yash', 20, 50.5);
 }
+
+
+
+
+
+void op (int a,int b,int c,int d){
+    int sum = (a+b);
+    print(sum);
+    int dev = (c-d);
+    print(dev);
+}
+void main(){
+    op (20,30,40,50);
+}
