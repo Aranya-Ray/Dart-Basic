9eacbb5bef73f848304a8291456e0fa3f8468180
Aranya Ray <aranyaray10@gmail.com>
2026-02-12 15:15:42 +0600

    Update functions.dart

--- functions.dart
+++ functions.dart
@@ -19,3 +19,71 @@ void op (int a,int b,int c,int d){
 void main(){
     op (20,30,40,50);
 }
+
+
+
+
+
+void up (String a,String b,int c,int d){
+    int sum = (c+d);
+    print(sum);
+    print("$a is $b");
+}
+void main(){
+    up("Rose","beautiful", 60,70);
+}
+
+
+
+
+
+void ty (double a,int b,bool c){
+    double sum = (a+b);
+    print(sum);
+    print(c);
+}
+void main(){
+    ty(10.7,30,true);
+}
+
+
+
+
+
+double ty (double a,int b){
+    double sum = (a+b);
+    return sum;
+
+}
+void main(){
+    double re = ty(10.7,30);
+    print(re);
+    
+}
+
+
+
+
+
+double ui ( int a, int n){
+    double dev = (a / n);
+    return dev;
+}
+void main(){
+    double oo = ui(56,78);
+    print (oo);
+}
+
+
+
+
+
+
+String oi(String nam1, String nam2) {
+  return "$nam1 $nam2";
+}
+
+void main() {
+  print(oi("Arpo", "himel"));
+}
+
