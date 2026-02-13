c82ffaeadc3357e91d4da1183e07d28ca0eaaf1f
Aranya Ray <aranyaray10@gmail.com>
2026-02-13 15:58:43 +0600

    Create Switch case.dart

---
+++ Switch case.dart
@@ -0,0 +1,68 @@
+void main() {
+  var dayOfWeek = 7;
+  switch (dayOfWeek) {
+    case 1:
+        print("Day is Sunday.");
+        break;
+    case 2:
+        print("Day is Monday.");
+      break;
+    case 3:
+      print("Day is Tuesday.");
+      break;
+    case 4:
+        print("Day is Wednesday.");
+      break;
+    case 5:
+        print("Day is Thursday.");
+      break;
+    case 6:
+        print("Day is Friday.");
+      break;
+    case 7:
+        print("Day is Saturday.");
+      break;
+    default:
+        print("Invalid Weekday.");
+      break;
+  }
+}
+
+
+
+
+void main (){
+var asdf = 5;
+switch (asdf){
+    case 1:
+    print("one");
+    break;
+    case 2:
+    print("two");
+    break;
+    case 3:
+    print("three");
+    break;
+    case 4:
+    print("four");
+    break;
+    case 5:
+    print ("five");
+    break;
+    default:
+    print("invaild number");
+    break;
+   
+    
+  }
+    
+}
+
+
+
+
+
+
+
+
+
