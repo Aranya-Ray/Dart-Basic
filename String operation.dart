1f981573910b07ecfa80d8b4baaa727617f2914c
Aranya Ray <aranyaray10@gmail.com>
2026-02-13 16:43:09 +0600

    Create String operation.dart

---
+++ String operation.dart
@@ -0,0 +1,26 @@
+void main() { 
+  String address1 = "Florida"; // Here F is capital
+  String address2 = "TexAs"; // Here T and A are capital
+  print("Address 1 in uppercase: ${address1.toUpperCase()}"); 
+  print("Address 1 in lowercase: ${address1.toLowerCase()}"); 
+  print("Address 2 in uppercase: ${address2.toUpperCase()}"); 
+  print("Address 2 in lowercase: ${address2.toLowerCase()}"); 
+}
+
+
+
+void main(){
+    String gh = "Arpo";
+    print("UpperCase:${gh.toUpperCase()}");
+    print("LowerCase:${gh.toLowerCase()}");
+}
+
+
+void main(){
+    String ufgj = "Himul Sarder";
+    String fjk = "Neloy Roy";
+    print("UpperCase:${ufgj.toUpperCase()}");
+    print("LowerCase:${ufgj.toLowerCase()}");
+    print("UpperCase:${fjk.toUpperCase()}");
+    print("LowerCase:${fjk.toLowerCase()}");
+}
