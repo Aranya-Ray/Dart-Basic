eff0ee44c7290237ceb158090aa05669b842428d
Aranya Ray <aranyaray10@gmail.com>
2026-02-13 14:53:08 +0600

    Create while loop.dart

---
+++ while loop.dart
@@ -0,0 +1,16 @@
+void main(){
+  int i = 0;
+  while(i <= 10){
+      print('$i');
+      i++;
+  }
+}
+
+
+void main(){
+    int l = 30;
+    while (l>=20){
+        print("$l");
+        l-=2;
+    }
+}
