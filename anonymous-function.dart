2c3ba969241c51f83726583ffe0625cb99f6bd4f
Aranya Ray <aranyaray10@gmail.com>
2026-02-14 23:48:46 +0600

    Create anonymous-function.dart

---
+++ anonymous-function.dart
@@ -0,0 +1,29 @@
+void main() {
+  const fruits = ["Apple", "Mango", "Banana", "Orange"];
+
+  fruits.forEach((fruit) {
+    print(fruit);
+  });
+}
+
+
+
+void main (){
+    const number = [67.9,89.9,56.0,45.9];
+    number.forEach((ok){
+        print(ok);
+    });
+}
+
+
+
+void main(){
+    const number = [1,6,7,5,8];
+    int sum = 0;
+    number.forEach((fjf){
+        sum = sum+fjf;
+        print(sum);
+        
+         });
+    
+}
