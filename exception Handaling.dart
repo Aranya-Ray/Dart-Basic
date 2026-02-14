0d002ea40df51a8483a412ffcc837de96c626eaf
Aranya Ray <aranyaray10@gmail.com>
2026-02-15 00:38:23 +0600

    Create exception Handaling.dart

---
+++ exception Handaling.dart
@@ -0,0 +1,28 @@
+void main() {   
+  int a = 18;   
+  int b = 0;   
+     
+  try {    
+      int res = a ~/ b;
+      print("Result is $res");   
+  }    
+     
+  catch(ex) {   
+      print(ex);   
+    }   
+} 
+
+
+
+void main(){
+    int a = 50;
+    int b = 0;
+    try {
+        int opp = a ~/ b;
+        print("Result is $opp");
+        
+    }
+    catch(ex){
+        print(ex);
+    }
+}
